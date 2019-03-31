1d3527ef22282a871f70d079e90e0c5b09a2a854
Alexandre Santos <mrxandejp@gmail.com>
2019-03-30 23:23:28 -0300

    Geração de nó

--- main.py
+++ main.py
@@ -6,14 +6,37 @@ Prof: Fernando Menezes Matos
 import node as no
 import packet as pk 
 import pyshicalLayer as pys
+import random
+from scipy.spatial import distance_matrix
+
+def GenerateNetwork(nodes,size, n): # Função que cria a topologia da rede
+    lista = []
+    for i in range(nodes):
+        while(True):
+            rx = random.randint(0, size)
+            ry = random.randint(0, size)
+            r = [rx, ry]
+            if r not in lista: 
+                lista.append(r)
+                n.append(no.Node(i,rx,ry))
+                break
 
 
-def GenerateNetwork(nodes,range,size): # Função que cria a topologia da rede
-	for i in range(nodes):
-		n[i] = no.Node(i,random.randint(0,size),random.randint(0,size))
+    #for i in range(nodes):
+	#	n[i] = no.Node(i,random.randint(0,size),random.randint(0,size))
 
+n = []
+GenerateNetwork(100,10, n)
+
+for i in n:
+    print(str(i.pos))
+
+
+
+'''
 no1 = no.Node(1,2,3)
 pkt1 = pk.Packet(10, [0,1,1])
 camadaFisica = pys.PyshicalLayer()
 
 camadaFisica.Send(pkt1)
+'''
